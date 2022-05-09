be8a34718b91561ddd5ab2284374803b927a8708
Mohammad99Azim <azim99mohammad@gmail.com>
2022-05-09 20:02:28 +0300

    for task 14 adding three url update delete and find specific movie

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS themove;
 
 CREATE TABLE IF NOT EXISTS themove (
-    id varchar(255),
+    id SERIAL PRIMARY KEY,
     title varchar(255),
     releasedate varchar(255),
     posterpath varchar(255)
