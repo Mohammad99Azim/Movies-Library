6518293dccb024175ff7cf4f3e7bfa8e3bd996f6
Mohammad99Azim <azim99mohammad@gmail.com>
2022-05-10 20:22:26 +0300

    correct the api key in the dotenv file

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const { Client } = require('pg');
-//const client = new Client(process.env.DATABASE_URL);
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
+const client = new Client(process.env.DATABASE_URL);
+// const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// })
 
 app.get('/', homeHanldler);
 app.get('/favorite', favoriteHanldler);
